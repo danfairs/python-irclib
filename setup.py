4a01cfeefccd2b4f458a2d0595c1e5ffb12c5176
Dan Fairs <dan@fezconsulting.com>
2011-08-08 18:37:06 +0100

    Setuptools.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+from setuptools import setups
+version = 0.0.0
+
+setup(name="python-irclib2",
+      version=version,
+      py_modules=["irclib", "ircbot"],
+      author="Joel Rosdahl",
+      author_email="jsoel@rosdahl.net",
+      url="http://python-irclib.sourceforge.net",
+      zip_safe=False,s
+    )
